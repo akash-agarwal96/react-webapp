91088d800cc61a8ff8ba964e26d581a574da11a7
Akash Agarwal <akashbittu96@gmail.com>
2020-07-24 00:39:02 +0530

    dishdetail

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import Dishdetail from './DishdetailComponent';
+
 
 class Menu extends Component{
     constructor(props){
@@ -14,24 +16,7 @@ class Menu extends Component{
         this.setState({selectedDish: dish});
     }
 
-    renderDish(dish){
-        if (dish != null){
-            return(
-                <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                </CardBody>
-                </Card>
-            );
-        }
-        else {
-            return (
-                <div></div>
-            )
-        }
-    }
+   
 
     render()
     {
@@ -54,10 +39,8 @@ class Menu extends Component{
               <div className="row">
                    {menu}
               </div>
-                <div className="row">
-                   {this.renderDish(this.state.selectedDish)}
-                </div>
-           </div>
+                  <Dishdetail dish= {this.state.selectedDish}></Dishdetail>
+            </div>
         );
     }
 }
